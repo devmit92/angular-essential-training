ac65e2907560363eaa534bf85e49268830f67ae8
Devon Mitchell <dmitchell01@321moonshot.com>
2019-10-09 08:04:34 -0500

    base of project started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [],
-  declarations: [],
-  bootstrap: [],
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 
 export class AppModule {}
